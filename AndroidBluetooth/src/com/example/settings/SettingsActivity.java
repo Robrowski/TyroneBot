21b286238d8b73a15fbb590a7848bfa5a4757296
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-01-16 22:18:00 -0500

    updated settings

--- AndroidBluetooth/src/com/example/settings/SettingsActivity.java
+++ AndroidBluetooth/src/com/example/settings/SettingsActivity.java
@@ -3,11 +3,11 @@ package com.example.settings;
 import android.app.Activity;
 import android.os.Bundle;
 
+//TODO why doesn't the action bar with up navigation show up....
 public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		// Display the fragment as the main content.
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new SettingsFragment()).commit();
